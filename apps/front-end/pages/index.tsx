e982bd8ad602c35470116689cefd644e067e5924
betty713 <betty.chen713@gmail.com>
2023-03-03 00:46:32 +0800

    implement Home page

--- apps/front-end/pages/index.tsx
+++ apps/front-end/pages/index.tsx
@@ -1,12 +1,121 @@
-import styles from './index.module.css'
-
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import './index.module.css'
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  return <h1 className="border-blue-600 ext-3xl font-bold underline">Home Page</h1>
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(null)
+  const onChange = (dates) => {
+    const [start, end] = dates
+    setStartDate(start)
+    setEndDate(end)
+  }
+
+  return (
+    <div
+      className="bg-cover bg-center h-screen"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1521747116042-5a810fda9664')",
+      }}
+    >
+      <div className="container mx-auto px-6">
+        <h1 className="text-6xl font-bold text-white mb-10">
+          即刻預定非凡旅程
+        </h1>
+        <form className="w-full max-w-lg mx-auto">
+          <div className="flex flex-wrap flex-col -mx-3 mb-6 md:flex-row">
+            <div className="md:w-1/3 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-city"
+              >
+                旅遊地點
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-city"
+                type="text"
+                value="台北"
+                disabled
+              />
+            </div>
+            <div className="md:w-1/2 px-3 mb-6">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-start-date"
+              >
+                入住日期
+              </label>
+              <div className="relative">
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-start-date"
+                  type="date"
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9 11l3-3 3 3h-6z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="absolute z-10">
+                <DatePicker
+                  selected={startDate}
+                  onChange={onChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  selectsRange
+                  inline
+                />
+              </div>
+            </div>
+            <div className="px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-guests"
+              >
+                入住人數
+              </label>
+              <div className="relative">
+                <select
+                  className="block appearance-none w-full bg-gray-200 border text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-guests"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9 11l3-3 3 3h-6z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center mt-10">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              搜尋
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Index
