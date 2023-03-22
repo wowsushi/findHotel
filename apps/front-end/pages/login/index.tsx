140ee4d7a02220298fc4ac7f7f35033a93068af8
betty713 <betty.chen713@gmail.com>
2023-03-22 10:24:44 +0800

    test

--- apps/front-end/pages/login/index.tsx
+++ apps/front-end/pages/login/index.tsx
@@ -27,7 +27,7 @@ const Login = () => {
       <div className="w-full max-w-md space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            登入..
+            登入
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={onSubmit}>
