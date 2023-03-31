3e61268ff2e9f5b196f4078505090772f700313f
betty713 <betty.chen713@gmail.com>
2023-03-31 17:59:46 +0800

    test

--- apps/front-end/pages/_app.tsx
+++ apps/front-end/pages/_app.tsx
@@ -83,8 +83,8 @@ function CustomApp({
 
 CustomApp.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx)
-  const { data } = await client.get('/auth/whoami')
-
+  // const { data } = await client.get('/auth/whoami')
+  const data = {}
   let pageProps = {}
 
   if (appContext.Component.getInitialProps) {
