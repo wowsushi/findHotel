10625a1458c32d5489b9ea65bbe67d3053ef58c4
betty713 <betty.chen713@gmail.com>
2023-03-31 12:27:44 +0800

    adjust cors setting and CSS

--- apps/back-end/src/main.ts
+++ apps/back-end/src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
-      origin: /^http:\/\/localhost:/,
+      origin: /^http:\/\/(localhost:)|(find-hotel\.online)/,
       credentials: true,
     },
   })
