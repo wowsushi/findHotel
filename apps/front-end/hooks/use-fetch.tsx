23b1714e155f3f924d8afae1b1c8b570c93e88dc
betty713 <betty.chen713@gmail.com>
2023-04-03 00:41:29 +0800

    test

--- apps/front-end/hooks/use-fetch.tsx
+++ apps/front-end/hooks/use-fetch.tsx
@@ -17,36 +17,36 @@ export const useFetch = () => {
   const [errors, setErrors] = useState(null)
   const [loading, setLoading] = useState(false)
 
-  const doRequest = async ({ onSuccess, ...props }: RequestProps) => {
-    console.log(process.env.NX_BASE_URL_CLIENT)
-    setLoading(true)
+  const doRequest = useCallback(
+    async ({ onSuccess, ...props }: RequestProps) => {
+      setLoading(true)
 
-    try {
-      setErrors(null)
-      const response = await instance(props)
-      console.log('instance', instance)
-      console.log('response', response)
-      if (onSuccess) {
-        onSuccess(response.data)
-      }
+      try {
+        setErrors(null)
+        const response = await instance(props)
+        if (onSuccess) {
+          onSuccess(response.data)
+        }
 
-      return response.data
-    } catch (err) {
-      // server side throw error
-      if (err.response.data.message) {
-        Modal.alert(err.response.data)
-        setErrors(err.response.data)
-        return err.response.data
-        // client side throw error
-      } else {
-        debugger
+        return response.data
+      } catch (err) {
+        // server side throw error
+        if (err.response.data.message) {
+          Modal.alert(err.response.data)
+          setErrors(err.response.data)
+          return err.response.data
+          // client side throw error
+        } else {
+          debugger
+        }
+      } finally {
+        setTimeout(() => {
+          setLoading(false)
+        }, 500)
       }
-    } finally {
-      setTimeout(() => {
-        setLoading(false)
-      }, 500)
-    }
-  }
+    },
+    []
+  )
 
   return { doRequest, errors, loading }
 }
