6f9f75c9caa77f8f0426206186c04e94b7e13d8f
betty713 <betty.chen713@gmail.com>
2024-03-20 10:28:46 +0800

    update config

--- apps/back-end/ormconfig.ts
+++ apps/back-end/ormconfig.ts
@@ -14,7 +14,6 @@ const dbConfig = {
   charset: 'utf8mb4',
   collation: 'utf8mb4_general_ci',
 }
-console.log(process.env.DB_NAME, process.env.DB_PASSWORD)
 switch (process.env.NODE_ENV) {
   case 'development':
     Object.assign(dbConfig, {
